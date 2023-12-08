d8765703aef3f3cbddaa40643b2018e42a0c4205
Heitor Costard <hlcostard@gmail.com>
2023-12-07 21:24:47 -0300

    Day6 solution

--- 2023/main.go
+++ 2023/main.go
@@ -3,9 +3,10 @@ package main
 import "aoc2023/solutions"
 
 func main() {
-  solutions.Day1()
-  solutions.Day2()
-  solutions.Day3()
-  solutions.Day4()
-  solutions.Day5()
+  // solutions.Day1()
+  // solutions.Day2()
+  // solutions.Day3()
+  // solutions.Day4()
+  // solutions.Day5()
+  solutions.Day6()
 }
---
+++ 2023/solutions/day6.go
@@ -0,0 +1,80 @@
+package solutions
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"aoc2023/utils"
+)
+
+func Day6() {
+  start := time.Now()
+
+  lines, err := utils.ReadLines("input/day6.txt")
+  if err != nil {
+    fmt.Printf("error reading file %v\n", err)
+  }
+  _ = lines
+
+  part1 := beatRecords(lines)
+  part2 := actuallyItsJustBeatRecord(lines)
+
+  elapsed := time.Since(start)
+  fmt.Printf("Day 06 part1 %v\t", part1)
+  fmt.Printf("part2 %v\ttime %v \n", part2, elapsed)
+}
+
+// part1
+func beatRecords(input []string) int {
+
+  var times []int
+  var distances []int
+
+  for i := 1; i < len(strings.Fields(input[0])); i++ {
+    time, _ := strconv.Atoi(strings.Fields(input[0])[i])
+    distance, _ := strconv.Atoi(strings.Fields(input[1])[i])
+    times = append(times, time)
+    distances = append(distances, distance)
+  }
+
+  result := 1
+  for i := range times {
+    result *= countTimesCanBeatRecord(times[i], distances[i])
+  }
+
+  return result
+}
+
+func actuallyItsJustBeatRecord(input []string) int {
+
+  time := ""
+  distance := ""
+
+  for i := 1; i < len(strings.Fields(input[0])); i++ {
+    time = time + strings.Fields(input[0])[i]
+    distance = distance + strings.Fields(input[1])[i]
+  }
+
+  timeInt, _ := strconv.Atoi(time)
+  distanceInt, _ := strconv.Atoi(distance)
+  
+  return countTimesCanBeatRecord(timeInt, distanceInt)
+}
+
+func countTimesCanBeatRecord(totalTime, distanceRecord int) int {
+  count := 0
+  distance := 0
+  for speed := 1; speed < totalTime; speed++ {
+    time := totalTime - speed 
+    distance = speed * (time)
+    if distance > distanceRecord {
+      count++
+    }
+  }
+
+  return count
+}
+
+
